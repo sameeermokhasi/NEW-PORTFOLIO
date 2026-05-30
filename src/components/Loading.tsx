05efb35b162a17b1e33b6a1ff9feccce7fcd88df
sameeermokhasi <sameermokhasi022@gmail.com>
2026-05-31 00:31:27 +0530

    Remove Logo text from loading header

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -46,7 +46,6 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          Logo
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
